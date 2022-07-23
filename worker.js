83bbf7181445fc017a7aa6c9f5f12b9924e58ef8
Nathan Clevenger <nateclev@gmail.com>
2022-07-23 03:46:50 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,3 +1,37 @@
+class ElementHandler {
+  element(element) {
+    // An incoming element, such as `div`
+    console.log(`Incoming element: ${element.tagName}`);
+  }
+}
+
+const getAttribute = async (attribute) => {
+    class AttributeScraper {
+      constructor(attr) {
+        this.attr = attr
+      }
+
+      element(element) {
+        if (this.value) return
+
+        this.value = element.getAttribute(this.attr)
+      }
+    }
+
+    const scraper = new AttributeScraper(attribute)
+
+    await new HTMLRewriter().on(this.selector, scraper).transform(this.response).arrayBuffer()
+
+    return scraper.value || ''
+}
+
 export default {
-  fetch: (req) => fetch('https://workers.cloudflare.com/cf.json', req.clone())
+  fetch: async (req) => {
+    
+    let url = new URL(request.url).pathname.replace('/','')
+    if (url && !url.match(/^[a-zA-Z]+:\/\//)) url = 'http://' + url
+    
+    const nextjsPage = await fetch(url)
+    
+  }
 }
